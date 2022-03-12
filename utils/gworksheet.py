6e5e7212c273b0266e1f0717bdac80c689853090
msramalho <19508417+msramalho@users.noreply.github.com>
2022-03-12 19:56:00 +0100

    fixes header offset

--- utils/gworksheet.py
+++ utils/gworksheet.py
@@ -18,8 +18,8 @@ class GWorksheet:
 
     def __init__(self, worksheet, columns=COLUMN_NAMES, header_row=1):
         self.wks = worksheet
-        self.values = self.wks.get_values()
-        self.headers = [v.lower() for v in self.values[header_row - 1]]
+        self.values = self.wks.get_values()[header_row-1:]
+        self.headers = [v.lower() for v in self.values[0]]
         self.columns = columns
 
     def _check_col_exists(self, col: str):
