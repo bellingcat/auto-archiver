4b423dfc34ec82c6f7678c59fe5f193f1bb7f028
msramalho <19508417+msramalho@users.noreply.github.com>
2022-06-27 14:36:58 +0200

    fix telethon exception

--- archivers/base_archiver.py
+++ archivers/base_archiver.py
@@ -93,16 +93,19 @@ class Archiver(ABC):
             return mime.split("/")[0]
         return ""
 
-    # eg images in a tweet save to cloud storage
+    def download_from_url(self, url, to_filename):
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
+        }
+        d = requests.get(url, headers=headers)
+        with open(to_filename, 'wb') as f:
+            f.write(d.content)
 
     def generate_media_page(self, urls, url, object):
         """
         For a list of media urls, fetch them, upload them
         and call self.generate_media_page_html with them
         """
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
-        }
 
         thumbnail = None
         uploaded_media = []
@@ -110,11 +113,7 @@ class Archiver(ABC):
             key = self._get_key_from_url(media_url, ".jpg")
 
             filename = os.path.join(Storage.TMP_FOLDER, key)
-
-            d = requests.get(media_url, headers=headers)
-            with open(filename, 'wb') as f:
-                f.write(d.content)
-
+            self.download_from_url(media_url, filename)
             self.storage.upload(filename, key)
             hash = self.get_hash(filename)
             cdn_url = self.storage.get_cdn_url(key)
