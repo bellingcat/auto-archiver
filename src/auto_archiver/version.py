8624e9f17731d082f9ea8e54d63eb35ea1fd2642
msramalho <19508417+msramalho@users.noreply.github.com>
2023-11-13 11:58:43 +0100

    version update 0.7.1

--- src/auto_archiver/version.py
+++ src/auto_archiver/version.py
@@ -1,9 +1,9 @@
 
 _MAJOR = "0"
-_MINOR = "6"
+_MINOR = "7"
 # On main and in a nightly release the patch should be one ahead of the last
 # released build.
-_PATCH = "13"
+_PATCH = "1"
 # This is mainly for nightly builds which have the suffix ".dev$DATE". See
 # https://semver.org/#is-v123-a-semantic-version for the semantics.
 _SUFFIX = ""
