71fcf5a089a134cf57cff41d72bd303f9e8d45bc
Chu-An, Huang <dreamcastle.andy@gmail.com>
2024-02-01 23:02:04 +0800

    fix: Correct the path of service account in google drive settings (#123)
    * fix: Correct the path of service account in yaml file
    
    * fix: Remove redefined function
    
    * Update src/auto_archiver/storages/gd.py
    
    * fix: remove unwanted drafting code
    
    ---------
    
    Co-authored-by: Miguel Sozinho Ramalho <19508417+msramalho@users.noreply.github.com>

--- src/auto_archiver/storages/gd.py
+++ src/auto_archiver/storages/gd.py
@@ -52,7 +52,7 @@ class GDriveStorage(Storage):
             else:
                 logger.debug('GD OAuth Token valid')
         else:
-            gd_service_account = config.service_account
+            gd_service_account = self.service_account
             logger.debug(f'Using GD Service Account {gd_service_account}')
             creds = service_account.Credentials.from_service_account_file(gd_service_account, scopes=SCOPES)
 
@@ -86,15 +86,6 @@ class GDriveStorage(Storage):
         # get id of file inside folder (or sub folder)
         file_id = self._get_id_from_parent_and_name(folder_id, filename)
         return f"https://drive.google.com/file/d/{file_id}/view?usp=sharing"
-
-    def upload(self, media: Media, **kwargs) -> bool:
-        # override parent so that we can use shutil.copy2 and keep metadata
-        dest = os.path.join(self.save_to, media.key)
-        os.makedirs(os.path.dirname(dest), exist_ok=True)
-        logger.debug(f'[{self.__class__.name}] storing file {media.filename} with key {media.key} to {dest}')
-        res = shutil.copy2(media.filename, dest)
-        logger.info(res)
-        return True
 
     def upload(self, media: Media, **kwargs) -> bool:
         logger.debug(f'[{self.__class__.name}] storing file {media.filename} with key {media.key}')
