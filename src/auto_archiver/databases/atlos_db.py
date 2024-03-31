10455edcf7ad91fd334c67410861e5fa5ad92e88
R. Miles McCain <github@sendmiles.email>
2024-03-31 11:55:19 +0200

    Add Atlos db

---
+++ src/auto_archiver/databases/atlos_db.py
@@ -0,0 +1,60 @@
+import os
+from typing import Union
+from loguru import logger
+from csv import DictWriter
+from dataclasses import asdict
+import requests
+
+from . import Database
+from ..core import Metadata
+from ..utils import get_atlos_config_options
+
+
+class AtlosDb(Database):
+    """
+    Outputs results to Atlos
+    """
+
+    name = "atlos_db"
+
+    def __init__(self, config: dict) -> None:
+        # without this STEP.__init__ is not called
+        super().__init__(config)
+
+    @staticmethod
+    def configs() -> dict:
+        return get_atlos_config_options()
+
+    def failed(self, item: Metadata, reason: str) -> None:
+        """Update DB accordingly for failure"""
+        # If the item has no Atlos ID, there's nothing for us to do
+        if not item.metadata["atlos_id"]:
+            logger.info(f"Item {item.get_url()} has no Atlos ID, skipping")
+            return
+
+        requests.post(
+            f"{self.atlos_url}/api/v2/source_material/metadata/{item.metadata['atlos_id']}/auto_archiver",
+            headers={"Authorization": f"Bearer {self.api_token}"},
+            json={"metadata": {"processed": True, "status": "error", "error": reason}},
+        ).raise_for_status()
+        logger.info(f"Stored failure for {item.get_url()} (ID {item.metadata['atlos_id']}) on Atlos: {reason}")
+
+    def fetch(self, item: Metadata) -> Union[Metadata, bool]:
+        """check and fetch if the given item has been archived already, each
+        database should handle its own caching, and configuration mechanisms"""
+        return False
+
+    def done(self, item: Metadata, cached: bool = False) -> None:
+        """archival result ready - should be saved to DB"""
+
+        if not item.metadata["atlos_id"]:
+            logger.info(f"Item {item.get_url()} has no Atlos ID, skipping")
+            return
+
+        requests.post(
+            f"{self.atlos_url}/api/v2/source_material/metadata/{item.metadata['atlos_id']}/auto_archiver",
+            headers={"Authorization": f"Bearer {self.api_token}"},
+            json={"metadata": {"processed": True, "status": "success"}},
+        ).raise_for_status()
+
+        logger.info(f"Stored success for {item.get_url()} (ID {item.metadata['atlos_id']}) on Atlos")
--- src/auto_archiver/feeders/atlos_feeder.py
+++ src/auto_archiver/feeders/atlos_feeder.py
@@ -27,18 +27,25 @@ class AtlosFeeder(Feeder):
             response = requests.get(
                 f"{self.atlos_url}/api/v2/source_material",
                 headers={"Authorization": f"Bearer {self.api_token}"},
-                params={"cursor": cursor}
+                params={"cursor": cursor},
             )
             data = response.json()
             response.raise_for_status()
             cursor = data["next"]
-            
+
             for item in data["results"]:
-                if item["source_url"] not in [None, ""]:
-                    yield Metadata().set_url(item["source_url"]).set("atlos_id", item["id"])
+                if (
+                    item["source_url"] not in [None, ""]
+                    and item["metadata"]
+                    .get("auto_archiver", {})
+                    .get("processed", False) != True
+                ):
+                    yield Metadata().set_url(item["source_url"]).set(
+                        "atlos_id", item["id"]
+                    )
                     count += 1
 
-            if len(data["results"]) == 0:
+            if len(data["results"]) == 0 or cursor is None:
                 break
 
         logger.success(f"Processed {count} URL(s)")
