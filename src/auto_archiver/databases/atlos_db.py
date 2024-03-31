26e8a137bbc685537e8a8ba23ed59025fb503b48
R. Miles McCain <github@sendmiles.email>
2024-04-01 01:52:40 +0200

    Formatting fixes

--- src/auto_archiver/databases/atlos_db.py
+++ src/auto_archiver/databases/atlos_db.py
@@ -37,12 +37,23 @@ class AtlosDb(Database):
             headers={"Authorization": f"Bearer {self.api_token}"},
             json={"metadata": {"processed": True, "status": "error", "error": reason}},
         ).raise_for_status()
-        logger.info(f"Stored failure for {item.get_url()} (ID {item.metadata['atlos_id']}) on Atlos: {reason}")
+        logger.info(
+            f"Stored failure for {item.get_url()} (ID {item.metadata['atlos_id']}) on Atlos: {reason}"
+        )
 
     def fetch(self, item: Metadata) -> Union[Metadata, bool]:
         """check and fetch if the given item has been archived already, each
         database should handle its own caching, and configuration mechanisms"""
         return False
+
+    def _process_metadata(self, item: Metadata) -> dict:
+        """Process metadata for storage on Atlos. Will convert any datetime
+        objects to ISO format."""
+
+        return {
+            k: v.isoformat() if hasattr(v, "isoformat") else v
+            for k, v in item.metadata.items()
+        }
 
     def done(self, item: Metadata, cached: bool = False) -> None:
         """archival result ready - should be saved to DB"""
@@ -54,7 +65,15 @@ class AtlosDb(Database):
         requests.post(
             f"{self.atlos_url}/api/v2/source_material/metadata/{item.metadata['atlos_id']}/auto_archiver",
             headers={"Authorization": f"Bearer {self.api_token}"},
-            json={"metadata": {"processed": True, "status": "success"}},
+            json={
+                "metadata": dict(
+                    processed=True,
+                    status="success",
+                    results=self._process_metadata(item),
+                )
+            },
         ).raise_for_status()
 
-        logger.info(f"Stored success for {item.get_url()} (ID {item.metadata['atlos_id']}) on Atlos")
+        logger.info(
+            f"Stored success for {item.get_url()} (ID {item.metadata['atlos_id']}) on Atlos"
+        )
