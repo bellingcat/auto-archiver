7c0dcbfd8125cee9b3306e74a9a7e922a006c20d
Patrick Robertson <robertson.patrick@gmail.com>
2025-01-21 16:49:30 +0100

    Re-add doc string to generic_archiver
    (renamed from youtube_archiver)

--- src/auto_archiver/archivers/generic_archiver/generic_archiver.py
+++ src/auto_archiver/archivers/generic_archiver/generic_archiver.py
@@ -1,3 +1,27 @@
+"""
+This is the generic archiver used by auto-archiver, which uses `yt-dlp` under the hood.
+
+This module is responsible for downloading and processing media content from platforms
+supported by `yt-dlp`, such as YouTube, Facebook, and others. It provides functionality
+for retrieving videos, subtitles, comments, and other metadata, and it integrates with
+the broader archiving framework.
+
+### Features
+- Supports downloading videos and playlists.
+- Retrieves metadata like titles, descriptions, upload dates, and durations.
+- Downloads subtitles and comments when enabled.
+- Configurable options for handling live streams, proxies, and more.
+
+### Dropins
+- For websites supported by `yt-dlp` that also contain posts in addition to videos
+ (e.g. Facebook, Twitter, Bluesky), dropins can be created to extract post data and create 
+ metadata objects. Some dropins are included in this generic_archiver by default, but
+custom dropins can be created to handle additional websites and passed to the archiver
+via the command line using the `--dropins` option (TODO!).
+
+"""
+
+
 import datetime, os, yt_dlp, pysubs2
 import importlib
 from typing import Type
