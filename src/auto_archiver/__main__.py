4a9fbd4e5f6de886ca5b2ddd766fb74cdab2c007
msramalho <19508417+msramalho@users.noreply.github.com>
2025-02-18 17:41:34 +0000

    two minor fixes

--- src/auto_archiver/__main__.py
+++ src/auto_archiver/__main__.py
@@ -3,7 +3,7 @@ from auto_archiver.core.orchestrator import ArchivingOrchestrator
 import sys
 
 def main():
-    for _ in ArchivingOrchestrator(). _command_line_run(sys.argv[1:]): pass
+    for _ in ArchivingOrchestrator()._command_line_run(sys.argv[1:]): pass
 
 if __name__ == "__main__":
     main()
