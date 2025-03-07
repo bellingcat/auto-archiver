40e5fe7a7ee6779753731294656db02d3d94e170
erinhmclark <erinhannahmary.clark@gmail.com>
2025-03-07 13:46:09 +0000

    Update __manifest__.py for merged Atlos module.

--- src/auto_archiver/modules/atlos_feeder_db_storage/__manifest__.py
+++ src/auto_archiver/modules/atlos_feeder_db_storage/__manifest__.py
@@ -19,23 +19,28 @@
         },
     },
     "description": """
-    AtlosFeederDbStorage: A module that integrates with the Atlos API to fetch source material URLs for archival, uplaod extracted media,
-    along with a database option to output archival results.
+    A module that integrates with the Atlos API to fetch source material URLs for archival, uplaod extracted media,
+    
+    [Atlos](https://www.atlos.org/) is a visual investigation and archiving platform designed for investigative research, journalism, and open-source intelligence (OSINT). 
+    It helps users organize, analyze, and store media from various sources, making it easier to track and investigate digital evidence.
+    
+    To get started create a new project and obtain an API token from the settings page. You can group event's into Atlos's 'incidents'.
+    Here you can add 'source material' by URLn and the Atlos feeder will fetch these URLs for archival.
+    
+    You can use Atlos only as a 'feeder', however you can also implement the 'database' and 'storage' features to store the media files in Atlos which is recommended.
+    The Auto Archiver will retain the Atlos ID for each item, ensuring that the media and database outputs are uplaoded back into the relevant media item.
+    
     
     ### Features
     - Connects to the Atlos API to retrieve a list of source material URLs.
-    - Filters source materials based on visibility, processing status, and metadata.
-    - Converts filtered source materials into `Metadata` objects with the relevant `atlos_id` and URL.
-    - Iterates through paginated results using a cursor for efficient API interaction.
-    - Outputs archival results to the Atlos API for storage and tracking.
-    - Updates failure status with error details when archiving fails.
-    - Processes and formats metadata, including ISO formatting for datetime fields.
-    - Skips processing for items without an Atlos ID.
-    - Saves media files to Atlos, organizing them into folders based on the provided path structure.
+    - Iterates through the URLs from all source material items which are unprocessed, visible, and ready to archive.
+    - If the storage option is selected, it will store the media files alongside the original source material item in Atlos.
+    - Is the database option is selected it will output the results to the media item, as well as updating failure status with error details when archiving fails.
+    - Skips Storege/ database upload for items without an Atlos ID - restricting that you must use the Atlos feeder so that it has the Atlos ID to store the results with.
 
     ### Notes
-    - Requires an Atlos API endpoint and a valid API token for authentication.
+    - Requires an Atlos account with a project and a valid API token for authentication.
     - Ensures only unprocessed, visible, and ready-to-archive URLs are returned.
-    - Handles pagination transparently when retrieving data from the Atlos API.
+    - Feches any media items within an Atlos project, regardless of separation into incidents.
     """
 }
