1f2d6379288c79e63a39dd25713b4d6055ff868d
msramalho <19508417+msramalho@users.noreply.github.com>
2025-06-08 14:16:21 +0100

    minor improvements

--- src/auto_archiver/modules/generic_extractor/generic_extractor.py
+++ src/auto_archiver/modules/generic_extractor/generic_extractor.py
@@ -371,7 +371,6 @@ class GenericExtractor(Extractor):
             data = ydl.extract_info(url, ie_key=info_extractor.ie_key(), download=True)
         except MaxDownloadsReached:  # proceed as normal once MaxDownloadsReached is raised
             pass
-        logger.success(data)
 
         if "entries" in data:
             entries = data.get("entries", [])
