a60d800b31fd0354858375841c492f363cdd0ec6
Dave Mateer <davemateer@gmail.com>
2025-06-16 15:07:39 +0100

    Changed log level for media

--- src/auto_archiver/core/media.py
+++ src/auto_archiver/core/media.py
@@ -116,7 +116,12 @@ class Media:
         # self.is_video() should be used together with this method
         try:
             streams = ffmpeg.probe(self.filename, select_streams="v")["streams"]
-            logger.warning(f"STREAMS FOR {self.filename} {streams}")
+            # DM 27th May 2025
+            # https://x.com/dave_mateer/status/1524341442738638848
+            # shows this warning for the gif file
+            # Have changed to debug for now to clean up logs
+            # logger.warning(f"STREAMS FOR {self.filename} {streams}")
+            logger.debug(f"STREAMS FOR {self.filename} {streams}")
             return any(s.get("duration_ts", 0) > 0 for s in streams)
         except Error:
             return False  # ffmpeg errors when reading bad files
